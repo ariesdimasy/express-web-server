6cd7f4a2f5430931d7181db52b3a27a9cfc0a755
ariesdimasy <ariesdimasy@gmail.com>
2021-07-25 21:06:14 +0700

    first init

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express")
+const app = express()
+const port = 4400
+
+app.get("/",(req, res) => {
+    res.send("Aries Dimas Y")
+})
+
+app.get("/contact",(req, res) => {
+    //res.send("Contact Page")
+    res.json({
+        name:"Aries Dimas Yudhistira",
+        email:"ariesdimasy@gmail.com",
+        
+    })
+});
+
+app.use("/",(req, res) => {
+    res.status(404)
+    res.send("404 Not Found")
+}) // middleware 
+
+app.listen(port, () => {
+    console.log(` this api listen to port ${port}`)
+})
