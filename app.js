adef840287e8053a674eb971dbb783d9d321863e
ariesdimasy <ariesdimasy@gmail.com>
2021-07-25 21:13:20 +0700

    change port

--- app.js
+++ app.js
@@ -1,25 +1,24 @@
-const express = require("express")
-const app = express()
-const port = 4400
+const express = require("express");
+const app = express();
+const port = 80;
 
-app.get("/",(req, res) => {
-    res.send("Aries Dimas Y")
-})
-
-app.get("/contact",(req, res) => {
-    //res.send("Contact Page")
-    res.json({
-        name:"Aries Dimas Yudhistira",
-        email:"ariesdimasy@gmail.com",
-        
-    })
+app.get("/", (req, res) => {
+  res.send("Aries Dimas Y");
 });
 
-app.use("/",(req, res) => {
-    res.status(404)
-    res.send("404 Not Found")
-}) // middleware 
+app.get("/contact", (req, res) => {
+  //res.send("Contact Page")
+  res.json({
+    name: "Aries Dimas Yudhistira",
+    email: "ariesdimasy@gmail.com",
+  });
+});
+
+app.use("/", (req, res) => {
+  res.status(404);
+  res.send("404 Not Found");
+}); // middleware
 
 app.listen(port, () => {
-    console.log(` this api listen to port ${port}`)
-})
+  console.log(` this api listen to port ${port}`);
+});
